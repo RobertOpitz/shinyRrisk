022b422f8565c8a69c5d83caf6ed3ad06b7be9fe
RobertOpitz <52924688+RobertOpitz@users.noreply.github.com>
2021-12-20 16:15:07 +0100

    updated version of starter script

--- shinyRrisk.R
+++ shinyRrisk.R
@@ -1,3 +1,34 @@
+dynamic_require <- function(package){
+  if (eval(parse(text = paste("require(",package,")")))) 
+    return(TRUE)
+  
+  install.packages(package, repos = "http://cran.rstudio.com")
+  return(eval(parse(text = paste("require(",package,")"))))
+}
+
+get_libraries <- function(packages) {
+  for (package in packages)
+    if (!dynamic_require(package)) 
+      install.packages(package, repos = "http://cran.rstudio.com")
+}
+
+get_libraries(c("DT",
+                "fitdistrplus",
+                "mc2d",
+                "network",
+                "networkD3",
+                "plotly",
+                "rmarkdown", "knitr",
+                "RJSONIO",
+                "shiny",
+                "shinyalert",
+                "shinycssloaders",
+                "shinydashboard",
+                "shinyFeedback",
+                "shinyjs",
+                "parallel", 
+                "mgcv"))
+
 #options(warn = 2, show.error.locations = "bottom")
 shiny::runApp('./inst/app.R', 
               port = 3838, 
